fc0a337d3e3b99331b12d584760bd82e8cbe6614
Sobindra2005 <sobhindra2005@gmail.com>
2024-09-14 00:06:55 -0400

    tailwind setup intial project structure setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
-import { useState } from 'react'
+import { useState } from "react";
 
 function App() {
-
   return (
     <>
- 
+      <div
+        className="border border-black text-gray-700" >
+        Hello world
+      </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
