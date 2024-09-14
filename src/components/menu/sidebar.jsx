4014f52743de47016621647b55ebc1d7e121d42d
Lagzen Thakuri <thakurizen2@gmail.com>
2024-09-14 18:38:43 +0545

    adding sidebar

---
+++ src/components/menu/sidebar.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+export default function Sidebar() {
+  return (
+    <>
+    <div>
+        <ul>
+            <li>Course</li>
+            <li>Business Planner</li>
+            <li>Market Trend</li>
+            <li>Community Forum</li>
+            <li>User Profile</li>
+        </ul>
+    </div>
+    </>
+  )
+}
