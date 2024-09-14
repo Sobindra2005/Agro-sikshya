0ca445253a4ec2917d7de3e4595748a5b249df37
Lagzen Thakuri <thakurizen2@gmail.com>
2024-09-14 16:23:02 +0545

    creating and adding signup

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,8 @@
 import Home from "./components/home";
-import Login from "./components/login";
+import Login from "./components/auth/login";
 import Header from "./components/menu/header";
 import Footer from "./components/menu/footer";
+import Signup from "./components/auth/signup";
 import {Routes, Route} from 'react-router-dom'
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/login" element={<Login />} />
+          <Route path="/signup" element={<Signup />} />
         </Routes>
       </div>
     </>
