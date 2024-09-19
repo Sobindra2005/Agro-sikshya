c5c2959afcbd7f632cb32c60a2e3c0761aa9f89d
Lagzen Thakuri <thakurizen2@gmail.com>
2024-09-19 15:22:13 +0545

    chaing imagee file names and

--- src/components/setting/sidebar.jsx
+++ src/components/setting/sidebar.jsx
@@ -4,7 +4,7 @@ export default function Sidebar() {
   return (
     <>
     <div className='  list-none space-y-10 m-2 p-2'>
-        <li><Link to='/'>Auth</Link></li>
+        <li><Link to='/login'>Auth</Link></li>
         <li><Link to='/'> General</Link></li>
         <li><Link to='/'> Plan</Link></li>
         <li><Link to='/'> Billing</Link></li>
