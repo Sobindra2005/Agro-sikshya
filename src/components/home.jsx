dec94dbdfac82b3f4f38b4a9d4e38e508c5cbc68
Sobindra2005 <sobhindra2005@gmail.com>
2024-09-21 02:09:43 -0700

    small

--- src/components/home.jsx
+++ src/components/home.jsx
@@ -10,7 +10,7 @@ import { FaUserCircle } from "react-icons/fa";
 export default function Home() {
   return (
     <>
-    <div className='relative w-full'>
+    <div className=' w-full'>
         <img src={A} alt="" />
 
         <div className='fixed top-0 right-0 left-56 bottom-0 '>
